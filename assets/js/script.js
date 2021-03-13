b5c9f0212cf11063c9071121b4c51c2195f39c6f
Muhammad Zahid <mrzahid@rogers.com>
2021-03-12 23:57:58 -0500

    Updating html, css and javascript files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,31 @@
 
-var wordArray = ["javascript", "function","variable","loop","object"];
+var wordArray = ["javascript", "function","variable","loop","object","boolean"];
 
 
 var wordInput = document.querySelector("#word");
 
-var startButton = document.querySelector("#start");
+var startButton = document.querySelector("#start_button");
+var timerEl = document.querySelector("#timerCount");
+var win = document.querySelector("#wins");
+var lose = document.querySelector("#loses");
+var wordBlank = document.querySelector("#result_msg");
+var resetButton = document.querySelector("#reset_button");
+
+
 var sWord = "";
 var dashStr = "";
+var winCounter = 0; var loseCounter = 0;  var timerCount = 10;
+var isWin = false;
 /*var msgDiv = document.querySelector("#msg");
-
-
-renderLastRegistered();
-
-function displayMessage(type, message) {
-  msgDiv.textContent = message;
-  msgDiv.setAttribute("class", type);
-}
-
-function renderLastRegistered() {
-  // TODO: Retrieve the last email and password and render it to the page
-  var email = localStorage.getItem("email");
-  var password = localStorage.getItem("password");
-  emailInput.value = email;
-  passwordInput.value = password;
 
 }*/
 
-startButton.addEventListener("click", function(event) {
-  event.preventDefault();
-  setWord();
-});
+startButton.addEventListener('click', setWord);
 
 function setWord(){
+  timerCount = 10;
+  isWin = false;
+  startButton.disabled = true;
   var windex = Math.floor((Math.random() * wordArray.length));  // +1
   sWord = wordArray[windex];
   var lw = sWord.length;
@@ -40,24 +34,52 @@ function setWord(){
   console.log("selected word :" + sWord);
   wordInput.value =  dashStr;
   wordInput.focus();
-
+  timerEl.textContent = timerCount;
+  startTimer();
 }
+
+// The setTimer function starts and stops the timer and triggers winGame() and loseGame()
+function startTimer() {
+  // Sets timer
+  var timer = setInterval(function() {
+    timerCount--;
+    timerEl.textContent = timerCount;
+    if (timerCount >= 0) {
+      // Tests if win condition is met
+      if (isWin && timerCount > 0) {
+        // Clears interval and stops timer
+        clearInterval(timer);
+        winGame();
+      }
+    }
+    // Tests if time has run out
+    if (timerCount === 0) {
+      // Clears interval
+      clearInterval(timer);
+      loseGame();
+    }
+  }, 1000);
+}
+
+
 wordInput.addEventListener("keydown",function(event){
-  event.stopPropagation();
+  //event.stopPropagation();
+  event.preventDefault();
+  if(timerCount <= 0){return false;}
   var key = event.key.toLowerCase();
   var alphabetNumericCharacters = "abcdefghijklmnopqrstuvwxyz0123456789 ".split("");
   if (alphabetNumericCharacters.includes(key)) {
-    /*elements.forEach(function(element) {
-      element.textContent += event.key;
-    });*/
-    console.log("selected letter : " + key);
-
+   // console.log("selected letter : " + key);
     if(sWord.indexOf(key) > -1){
       var lIndex = sWord.indexOf(key);
-      dashStr = replaceChar(dashStr, key, lIndex)
-      console.log("rep-dash : " + dashStr);
+      dashStr = replaceChar(sWord, dashStr, key, lIndex)
+     // console.log("rep-dash : " + dashStr);
       wordInput.value =  dashStr;
-      wordInput.focus();
+      if(sWord.toLowerCase() === dashStr.toLowerCase()){
+        isWin = true;
+      }else{
+        wordInput.focus();
+      }
     
     }
   }
@@ -65,12 +87,59 @@ wordInput.addEventListener("keydown",function(event){
   
 
 });
-function replaceChar(origString, replaceChar, index) {
-  let newStringArray = origString.split("");
-
-  newStringArray[index] = replaceChar;
-
-  let newString = newStringArray.join("");
-
+function replaceChar(origWord, dashString, replaceChar, index) {
+  origWord = origWord.toLowerCase();
+  
+  let wArray = origWord.split('');
+  let DashArray = dashString.split('');
+  for(var i=0; i<wArray.length; i++){
+    if(wArray[i] === replaceChar){
+      DashArray[i] = replaceChar;
+    }
+  }
+  let newString = DashArray.join('');
   return newString;
 }
+
+function winGame() {
+  wordBlank.textContent = "YOU WON!!!🏆 ";
+  winCounter++
+  startButton.disabled = false;
+  setWins()
+}
+
+// The loseGame function is called when timer reaches 0
+function loseGame() {
+  wordBlank.textContent = "GAME OVER";
+  loseCounter++
+  startButton.disabled = false;
+  setLosses()
+}
+function setWins() {
+  win.textContent = winCounter;
+  localStorage.setItem("winCount", winCounter);
+}
+
+// Updates lose count on screen and sets lose count to client storage
+function setLosses() {
+  lose.textContent = loseCounter;
+  localStorage.setItem("loseCount", loseCounter);
+}
+
+resetButton.addEventListener('click', function(){
+  loseCounter = 0;  winCounter = 0
+  localStorage.setItem("winCount", winCounter);
+  localStorage.setItem("loseCount", loseCounter);
+  win.textContent = winCounter;
+  lose.textContent = loseCounter;
+});
+
+function init(){
+  winCounter = localStorage.getItem("winCount");
+  if(winCounter === ""){winCounter = 0}
+  loseCounter = localStorage.getItem("loseCount");
+  if(loseCounter === ""){loseCounter = 0}
+  win.textContent = winCounter;
+  lose.textContent = loseCounter;
+}
+init();
